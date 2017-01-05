c5f15faa6cf19de37b7abb6ec95b0b9f2e6b5f79
Edgar Santos-Fernandez <edgar.santosfdez@gmail.com>
2017-01-06 11:53:11 +1300

    update report_point
    Hello @LJWu, thanks for spotting the issue. This should work. Please, test and let me know.
    Cheers

--- R/report_point.r
+++ R/report_point.r
@@ -22,6 +22,7 @@
 
 
 
+
 report_point <- function(period, acti_data, ...){
   particip <- as.vector(t(distinct(period, actigraphy_file)))
 
@@ -34,12 +35,12 @@ report_point <- function(period, acti_data, ...){
   colName <- c("Actisoft_ID",	"period_number",	"time_since_up", "time_awake",	"last_rest_end",	"last_sleep_end",	"with_excluded_bad",
                "with_forced_wake",	"with_forced_sleep",	"with_custom_interval", "point_overlap_sleep",	"point_overlap_bed")
 
-  report = data.frame(matrix(vector(), nrow(period), length(colName), dimnames = list(c(), colName)), stringsAsFactors = F)
-
+  report <- data.frame(matrix(vector(), nrow=1, length(colName), dimnames = list(c(), colName)), stringsAsFactors = F)
   report$last_rest_end <- as.POSIXct((report$last_rest_end))
   report$last_sleep_end <- as.POSIXct((report$last_sleep_end))
 
   y <- 1
+  report2 = NULL
   for (ii in 1 : length(particip)){
     tab1 <- dplyr::filter(bypart, analysis_name == particip[ii])
     tab2 <- dplyr::filter(period, actigraphy_file == particip[ii])
@@ -60,20 +61,22 @@ report_point <- function(period, acti_data, ...){
                   point_overlap = ifelse(datime_end >= tab2$time_point_datime[jj], TRUE, FALSE)
         )
 
-      report$Actisoft_ID[y] <- y
-      report$period_number[y] <- jj
-      report$time_since_up[y] <- difftime(tab2$time_point_datime[jj] , mat2$datime_end[1], units = "mins" )
-      report$time_awake[y] <- difftime(tab2$time_point_datime[jj] , mat2$datime_end[2], units = "mins" )
-      report$last_rest_end[y] <- ymd_hms( mat2$datime_end[1])
-      report$last_sleep_end[y] <- mat2$datime_end[2]
-      report$point_overlap_bed[y] <- mat4$point_overlap[1]
-      report$point_overlap_sleep[y] <- mat4$point_overlap[2]
+      report$Actisoft_ID <- y
+      report$period_number <- jj
+      report$time_since_up <- difftime(tab2$time_point_datime[jj] , mat2$datime_end[1], units = "mins" )
+      report$time_awake <- difftime(tab2$time_point_datime[jj] , mat2$datime_end[2], units = "mins" )
+      report$last_rest_end <- mat2$datime_end[1]
+      report$last_sleep_end <- mat2$datime_end[2]
+      report$point_overlap_bed <- mat4$point_overlap[1]
+      report$point_overlap_sleep <- mat4$point_overlap[2]
 
+      report2 <- rbind(report2, report)
       y <- y +  1
     }
 
   }
 
-  report2 <- cbind(period, report)
+  report2 <- cbind(period, report2)
   tbl_df(report2)
 }
+
