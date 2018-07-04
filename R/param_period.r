9cfe8adc4eb6dd1b4f07deab33e71ac1e4411ff4
ed_sans <edgar.santosfdez@gmail.com>
2018-07-04 17:34:28 +1000

    no longer used

--- R/param_period.r
+++ R/param_period.r
@@ -1,13 +1,1 @@
-#'Imports parameter periods
-#'
-#' @param filepath the path to the file
-#' @return a dataframe
-#' @export
 
-
-param_period <- function(filepath){
-    read.csv(file = paste(filepath), sep = ",", header = TRUE, skip = 0)
-}
-
-#fp <- 'input parameters example period.csv'
-#intput.param.period <- param_period (filepath = fp)
