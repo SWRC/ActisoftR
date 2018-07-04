acc950f4beeb3831ece63d237dd082191d1e0bbc
ed_sans <edgar.santosfdez@gmail.com>
2018-07-04 17:32:45 +1000

    Update int_startend.r

--- R/int_startend.r
+++ R/int_startend.r
@@ -14,21 +14,18 @@
 #'
 int_startend <- function(x, tz = "UTC", ...){
   subject_ID <- summary_start_datime <- summary_end_datime <- NULL
-# starting datime
   use <- c("subject_ID", "summary_start_datime", "summary_end_datime")
   use2 <- c("summary_start_datime", "summary_end_datime")
   y <- x[, use]
-mini <- as.POSIXct(apply(y[,use2], 1, min), tz = tz)#lubridate::ymd_hms(apply(y[,use2], 1, min))
-maxi <- as.POSIXct(apply(y[,use2], 1, max), tz = tz)#lubridate::ymd_hms(apply(y[,use2], 1, max))
+mini <- as.POSIXct(apply(y[,use2], 1, min), tz = tz)
+maxi <- as.POSIXct(apply(y[,use2], 1, max), tz = tz)
 y$summary_start_datime <- mini
 y$summary_end_datime <- maxi
 
   from <- y  %>% group_by(subject_ID) %>% slice(which.min(summary_start_datime))
   from <- select_(from, .dots = c("subject_ID", "summary_start_datime"))
-  # ending datime
   to <- y %>% group_by(subject_ID) %>% slice(which.max(summary_end_datime))
   to <- select_(to, .dots = c("subject_ID", "summary_end_datime"))
   from$summary_end_datime <-  to$summary_end_datime
   from
-#int_startend(period)
 }
